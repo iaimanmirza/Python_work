97bb453d3112a030aaafc170795c0affa12e3117
iaimanmirza <50889492+iaimanmirza@users.noreply.github.com>
2019-06-16 05:45:57 -0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,82 @@
+#greet() #function calling
+#print("I am outside the function")
+#greet()
+#print("Completed")
+
+
+#def add():
+ #   a = 2
+  #  b = 3
+   ## my_sum = a + b
+    ##print(my_sum)
+#add()
+
+#a = int(input("Enter number 1:"))
+#b = int(input("Enter number 2:"))
+#def add(num1 , num2): #Recieving information/data called parameter
+#    my_sum = num1 + num2
+#    print(my_sum)
+#add(a,b) # passing data called arguments
+
+#a = int(input("Enter number 1:"))
+#b = int(input("Enter number 2:"))
+#def sub(num1 , num2) :
+#    my_sum = num1 - num2
+#    print(my_sum)
+#sub(a,b)
+
+#a = int(input("Enter number 1:"))
+#b = int(input("Enter number 2:"))
+#def divide(num1 , num2) :
+#    my_sum = num1 / num2
+#    print(my_sum)
+#divide(a,b)
+
+#a = int(input("Enter number 1:"))
+#b = int(input("Enter number 2:"))
+#def multiply(num1 , num2) :
+#    my_sum = num1 * num2
+#    print(my_sum)
+#
+# multiply(a,b)
+
+#def my_pet(owner,pet , city= "Karachi"):
+#    print(owner , "is an owner of" , pet , "They are from ", city)
+#my_pet(pet = "Cat" , owner = "Sarah")
+#my_pet(pet = "Rabbit" , owner = "Ali" , city="Lahore") # here you dont have to consider position
+
+#there are 4 types of function
+#1 take nothing return nothing
+#2 take sth return nothing
+#3 take nothing return sth
+#4 take sth return sth
+
+#def sum():
+#   a=2
+#   b=3
+    #print ('sum is (a+b))
+#    return (a+b)
+#result = sum()
+#print(result)
+
+#def sum(val1, val2):
+#    result = val1+val2
+#    return result
+
+#a = int(input("Enter value 1"))
+#b = int(input("Enter value 2"))
+
+#def sum(val1, val2):
+#    result = val1+val2
+#    return result
+#output_of_function = sum(a,b)
+#print(output_of_function)
+
+#value1=int(input("enter a number"))
+
+#def check(val1):
+#    if(val1%2) == 0:
+#        print(val1,"number is even number")
+#    else:
+#       print(val1,"number is odd number")
+#check(value1)
